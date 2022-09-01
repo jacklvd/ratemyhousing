c30333fd66aa6f714732da04b0738e3c58a1662a
jacklvd <vodanglong3103@gmail.com>
2022-09-01 16:14:35 -0400

    update

--- backend/myhousing/myhousing/settings.py
+++ backend/myhousing/myhousing/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from Lib.secrets import SECRETS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s*)q+y)0w9*p&l0l5wdchu-q9gzc72p5m7%l)wt#m72m66n943'
+SECRET_KEY = SECRETS
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
