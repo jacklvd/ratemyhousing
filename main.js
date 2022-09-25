46524b2a852e74b8686cdfa091c1e2b6fadc5f8d
CatLuong0106 <72310549+CatLuong0106@users.noreply.github.com>
2022-09-25 16:40:05 -0400

    Add files via upload
    Frontend - Chou Commits

---
+++ main.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    window.addEventListener('scroll', function (e) {
+        if(window.pageYOffset >100) {
+            this.document.querySelector("header").classList.add('is-scrolling');
+        } else {
+            this.document.querySelector("header").classList.remove('is-scrolling');
+        }
+    });
+
+const hamburger = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.navmenu');
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+}
+
